a2e5b8eb8f61fbb964b5827d9f9f1ee1af0361e2
Fancyyy <18012298125@163.com>
2019-08-21 15:23:40 +0800

    shopcart组件动态变换

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,16 +3,17 @@
       <div class="content">
           <div class="content-left">
               <div class="logo-wrapper">
-                  <div class="logo">
-                      <i class="icon-shopping_cart"></i>
+                  <div class="logo" :class="{'highlight':totalCount>0}">
+                      <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
                   </div>
+                  <div class="num" v-show="totalCount>0">{{totalCount}}</div>
               </div>
-              <div class="price">￥0</div>
+              <div class="price" :class="{'highlight':totalPrice>0}">￥{{totalPrice}}</div>
               <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
           </div>
           <div class="content-right">
-              <div class="pay">
-                  ￥{{minPrice}}起送
+              <div class="pay" :class="payClass">
+                  {{payDesc}}
               </div>
           </div>
       </div>
@@ -22,6 +23,17 @@
 <script type='text/ecmascript-6'>
 export default {
     props: {
+        selectFoods: {
+            type: Array,
+            default() {
+                return [
+                    {
+                        price: 10,
+                        count: 1
+                    }
+                ];
+            }
+        },
         deliveryPrice: {
             type: Number,
             default: 0
@@ -29,6 +41,39 @@ export default {
         minPrice: {
             type: Number,
             default: 0
+        }
+    },
+    computed: {
+        totalPrice() {
+            let total = 0;
+            this.selectFoods.forEach((food) => {
+                total += food.price * food.count;
+            });
+            return total;
+        },
+        totalCount() {
+            let count = 0;
+            this.selectFoods.forEach((food) => {
+                count += food.count;
+            });
+            return count;
+        },
+        payDesc() {
+            if (this.totalPrice === 0) {
+                return `￥${this.minPrice}元起送`;
+            } else if (this.totalPrice < this.minPrice) {
+                let diff = this.minPrice - this.totalPrice;
+                return `还差￥${diff}元起送`;
+            } else {
+                return '去结算';
+            }
+        },
+        payClass() {
+            if (this.totalPrice < this.minPrice) {
+                return 'not-enough';
+            } else {
+                return 'enough';
+            }
         }
     }
 };
@@ -67,9 +112,27 @@ export default {
                         border-radius: 50%
                         text-align: center
                         background: rgba(255,255,255,0.4)
+                        &.highlight
+                            background: rgb(0,160,220)
                         .icon-shopping_cart
                             line-height: 44px
                             font-size: 24px
+                            &.highlight
+                                color: rgb(255,255,255)
+                    .num
+                        position: absolute
+                        top: 0
+                        right: 0
+                        width: 24px
+                        height: 16px
+                        line-height: 16px
+                        text-align: center
+                        border-radius: 16px
+                        font-size: 9px
+                        font-weight: 700
+                        color: #fff
+                        background: rgb(240,20,20)
+                        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4)
                 .price
                     display: inline-block
                     vertical-align: top
@@ -80,6 +143,8 @@ export default {
                     border-right: 1px solid rgba(255,255,255,0.1)
                     font-size: 16px
                     font-weight: 700
+                    &.highlight
+                        color: #fff
                 .desc
                     display: inline-block
                     vertical-align: top
@@ -95,5 +160,9 @@ export default {
                     text-align: center
                     font-size: 12px
                     font-weight: 700
-                    background: #2b333b
+                    &.not-enough
+                        background: #2b333b
+                    &.enough
+                        background: #00b43c
+                        color: #fff
 </style>
